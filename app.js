388a5dba5771145e5e5746bf4b4171b7cbe2ecfa
ykolambe <ykolambe123@gmail.com>
2024-07-15 16:27:17 +0400

    port changes

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.post('/webhook', async (req, res) => {
     const { from, name, text } = entry[0].changes[0].value.messages[0];
     console.log("ENTRYYYYY: ",entry);
 
-    var namea = entry.changes;//.contacts ; //.profile.name;
+    var namea = entry[0].changes;//.contacts ; //.profile.name;
     console.log(namea)
     // Insert data into RDS table
     await insertMessage(from, name, text);
