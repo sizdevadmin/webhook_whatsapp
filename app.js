d6d9834478592666a71878afd572f4b2e8f60f21
ykolambe <ykolambe123@gmail.com>
2024-07-15 16:04:08 +0400

    port changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
 const express = require('express');
 const mysql = require('mysql2/promise'); 
 
-//const app = express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON request bodies
